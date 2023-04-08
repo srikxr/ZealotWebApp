863f2562b22d84464d696691eb523bb49ec0a7fd
srikxr <srikarchava5@gmail.com>
2023-04-08 08:48:11 -0700

    New logo + auth_config file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="dev-617q62a0x1nxk4v2.us.auth0.com"
+      clientId="4M3BruRDDFosOim7bl3YQbywge3SdCN6"
+      redirectUri={typeof window !== 'undefined' && window.location.origin}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
